68568024854e9e4e5f8657f822a49f5bf2709b7d
이현규/WS팀/NE <hyungyu.lee@nhn.com>
2019-10-24 13:30:30 +0900

    [공통] Gradle 기반 멀티모듈 프로젝트 최초 구성

---
+++ withdaily-api/src/main/java/com/withdaily/api/WithdailyAPIApplication.java
@@ -0,0 +1,13 @@
+package com.withdaily.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithdailyAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithdailyAPIApplication.class, args);
+	}
+
+}
---
+++ withdaily-api/src/test/java/com/withdaily/api/WithdailyAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withdaily.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithdailyAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
